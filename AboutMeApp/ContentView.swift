a172dc42c850508d33d8e0d0d86ba9357d836083
Bread Beisheim <catcoder01@gmail.com>
2023-07-17 10:01:15 -0500

    First Commit

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -31,6 +31,7 @@ struct ContentView: View {
             
             if showBio {
                 Text("My name is Bread Melody Beisheim. I'm 13 years old and live in Austin TX with my dad and cat Annabell. I love animals, especially cats, and have an intrest in becoming a biologist or salesman. I'm transgender female to male and my pronouns are he/him. I've been out for 3 years, and socially transitioning for 2. I go to Ojeda Middle School and i'm a rising 8th grader. I've previously lived in St Louis MO, Portola Valley CA, and Bethesda MD. My dad is from Rochester NY and my mom is from Sofia, Bulgaria. I love music, plays, concerts, movies, and shows. I also like meditation and spirituality. I hope that someday I can make an impact on people! ")
+                    .font(.caption)
                     .multilineTextAlignment(.center)
                 .padding(8.0) }
             
